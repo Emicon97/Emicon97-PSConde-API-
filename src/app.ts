f9b39976cef3f68fc71eda831e9fb0a7361d2f92
EmiCon97 <derleuchtturm97@gmail.com>
2022-11-29 20:58:34 -0300

    cors url

--- src/app.ts
+++ src/app.ts
@@ -56,7 +56,7 @@ class App {
     this.app.use(morgan(LOG_FORMAT, { stream }));
     // this.app.use(cors({ origin: '*', credentials: CREDENTIALS }));
     this.app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+      res.header('Access-Control-Allow-Origin', 'https://psconde-com.onrender.com/home/*'); // update to match the domain you will make the request from
       res.header('Access-Control-Allow-Credentials', 'true');
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, auth-token, refresh-token, id');
       res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
