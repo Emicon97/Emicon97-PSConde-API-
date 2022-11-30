114e2e68c58e78848a1e5b470b31dbb78f93c548
EmiCon97 <derleuchtturm97@gmail.com>
2022-11-30 13:22:21 -0300

    cors url

--- src/app.ts
+++ src/app.ts
@@ -54,14 +54,14 @@ class App {
 
   private initializeMiddlewares() {
     this.app.use(morgan(LOG_FORMAT, { stream }));
-    // this.app.use(cors({ origin: '*', credentials: CREDENTIALS }));
-    this.app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-      res.header('Access-Control-Allow-Credentials', 'true');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, auth-token, refresh-token, id');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-      next();
-    })
+    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+    // this.app.use((req, res, next) => {
+    //   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    //   res.header('Access-Control-Allow-Credentials', 'true');
+    //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, auth-token, refresh-token, id');
+    //   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    //   next();
+    // })
     this.app.use(hpp());
     this.app.use(helmet());
     this.app.use(compression());
